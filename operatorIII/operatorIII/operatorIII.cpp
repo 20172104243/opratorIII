b90bf46811d920abba7146cc454a9a82e2c98d50
20172104243 <15049115743@163.com>
2018-05-28 09:59:46 +0800

    Initial commit

---
+++ operatorIII/operatorIII/operatorIII.cpp
@@ -0,0 +1,49 @@
+
+// operatorplus.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+class CFeet
+{
+private:
+	int feet;
+	int inches;
+public:
+	void setvalue(int fe, int in);
+	void display();
+	CFeet minus(CFeet & objk);
+	CFeet operator -(CFeet & objk);
+};
+void CFeet::setvalue(int fe, int in)
+{
+	feet = fe + in / 12;
+	inches = in % 12;
+}
+void CFeet::display()
+{
+	cout << feet << "英尺" << inches << "英寸" << endl;
+}
+CFeet CFeet::minus(CFeet & objk)
+{
+	CFeet temp;
+	temp.setvalue(feet - objk.feet, inches - objk.inches);
+	return temp;
+}
+CFeet CFeet ::operator -(CFeet & objk)
+{
+	CFeet temp;
+	temp.setvalue(feet - objk.feet, inches - objk.inches);
+	return temp;
+}
+int main()
+{
+	CFeet A, B, C;
+	A.setvalue(23, 24);
+	B.setvalue(3, 2);
+	C = A - B;
+	C.display();
+	return 0;
+}
+
